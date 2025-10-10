390d88dc899a687df32df1608177385350ddd78d
ZilloweZ <zillowez@gmail.com>
2025-10-11 00:00:15 +0300

    update

--- community/eza/eza.pkg.lua
+++ community/eza/eza.pkg.lua
@@ -1,13 +1,19 @@
 local repo_owner = "eza-community"
 local repo_name = "eza"
-local version = SYSTEM.VERSION or "0.23.3"
+local version = SYSTEM.VERSION or "0.23.4" -- Use a real latest version as default
 local git_url = "https://github.com/" .. repo_owner .. "/" .. repo_name .. ".git"
 
-package({
+local bins = { "eza" }
+if SYSTEM.OS == "windows" then
+	bins = { "eza.exe" }
+end
+
+metadata({
 	name = repo_name,
 	repo = "community",
 	version = version,
-	description = "eza is a modern alternative for the venerable file-listing command-line program ls that ships with Unix and Linux operating systems, giving it more features and better defaults",
+	description =
+	"eza is a modern alternative for the venerable file-listing command-line program ls that ships with Unix and Linux operating systems, giving it more features and better defaults",
 	website = "https://eza.rocks/",
 	git = git_url,
 	maintainer = {
@@ -19,22 +25,10 @@ package({
 		name = "Christina Sørensen",
 		email = "christina@cafkafk.com",
 	},
-	license = "EUPL-1.2",
-	bins = { "eza" },
-	conflicts = { "eza" },
-})
-
-install({
-	{
-		name = "Build from source",
-		type = "source",
-		url = PKG.git,
-		platforms = { "all" },
-		build_commands = {
-			"cargo build --release",
-		},
-		bin_path = "target/release/eza",
-	},
+	license = "MIT",
+	bins = bins,
+	conflicts = { "eza", "exa" },
+	types = { "prebuilt", "source" },
 })
 
 dependencies({
@@ -42,3 +36,66 @@ dependencies({
 		required = { "native:cargo" },
 	},
 })
+
+function prepare()
+	if BUILD_TYPE == "prebuilt" then
+		local arch = SYSTEM.ARCH
+		if arch == "amd64" then
+			arch = "x86_64"
+		elseif arch == "arm64" then
+			arch = "aarch64"
+		end
+
+		local os = SYSTEM.OS
+		if os == "macos" then
+			os = "darwin"
+		end
+
+		local platform_key = arch .. "-" .. os
+		local platform_map = {
+			["x86_64-linux"] = "x86_64-unknown-linux-gnu",
+			["aarch64-linux"] = "aarch64-unknown-linux-gnu",
+			["x86_64-windows"] = "x86_64-pc-windows-gnu",
+		}
+		local target = platform_map[platform_key]
+		if not target then
+			error("Unsupported platform for prebuilt binary: " .. platform_key)
+		end
+
+		local extension = ".tar.gz"
+		if platform_key == "x86_64-windows" then
+			extension = ".zip"
+		end
+
+		local asset_name = "eza-" .. target .. extension
+		local url = "https://github.com/"
+				.. repo_owner
+				.. "/"
+				.. repo_name
+				.. "/releases/download/v"
+				.. PKG.version
+				.. "/"
+				.. asset_name
+
+		UTILS.EXTRACT(url, "prebuilt")
+	end
+end
+
+function package()
+	local bin_name = "eza"
+	if SYSTEM.OS == "windows" then
+		bin_name = "eza.exe"
+	end
+
+	if BUILD_TYPE == "prebuilt" then
+		local bin_path = UTILS.FIND.file("prebuilt", bin_name)
+		if bin_path then
+			zcp(bin_path, "${pkgstore}/bin/" .. bin_name)
+		else
+			error("Could not find '" .. bin_name .. "' binary in extracted archive")
+		end
+	elseif BUILD_TYPE == "source" then
+		cmd("cargo build --release")
+		zcp("target/release/" .. bin_name, "${pkgstore}/bin/" .. bin_name)
+	end
+end
--- zillowe/hello/hello.pkg.lua
+++ zillowe/hello/hello.pkg.lua
@@ -1,6 +1,6 @@
 local repo_owner = "Zillowe"
 local repo_name = "Hello"
-local version = SYSTEM.VERSION or "3.0.0"
+local version = ZOI.VERSION or "3.0.0"
 local git_url = "https://github.com/" .. repo_owner .. "/" .. repo_name .. ".git"
 local release_base_url = "https://github.com/" .. repo_owner .. "/" .. repo_name .. "/releases/download/v" .. version
 
@@ -14,10 +14,10 @@ local function get_mapped_platform()
 end
 
 local function get_mapped_os()
-	return get_mapped_platform():match("([^%-]+)")
+	return get_mapped_platform():match("([^-]+)")
 end
 
-package({
+metadata({
 	name = "hello",
 	repo = "zillowe",
 	version = version,
@@ -34,101 +34,11 @@ package({
 		name = "Zillowe Foundation",
 		website = "https://zillowe.qzz.io",
 		email = "contact@zillowe.qzz.io",
-		key = "https://zillowe.pages.dev/keys/zillowe-main.asc",
-		key_name = "zillowe-main",
 	},
 	license = "Apache-2.0",
 	bins = { "hello" },
 	conflicts = { "hello" },
-})
-
-install({
-	selectable = true,
-	{
-		name = "Binary",
-		type = "binary",
-		url = (function()
-			return release_base_url .. "/hello-" .. get_mapped_os() .. "-" .. SYSTEM.ARCH
-		end)(),
-		platforms = { "all" },
-		checksums = (function()
-			return release_base_url .. "/checksums-512.txt"
-		end)(),
-		sigs = {
-			{
-				file = (function()
-					return "hello-" .. get_mapped_os() .. "-" .. SYSTEM.ARCH
-				end)(),
-				sig = (function()
-					return release_base_url .. "/hello-" .. get_mapped_os() .. "-" .. SYSTEM.ARCH .. ".sig"
-				end)(),
-			},
-		},
-	},
-	{
-		name = "Compressed Binary",
-		type = "com_binary",
-		url = (function()
-			local ext
-			if SYSTEM.OS == "windows" then
-				ext = "zip"
-			else
-				ext = "tar.xz"
-			end
-			return release_base_url .. "/hello-" .. get_mapped_os() .. "-" .. SYSTEM.ARCH .. "." .. ext
-		end)(),
-		platforms = { "all" },
-		checksums = (function()
-			return release_base_url .. "/checksums-512.txt"
-		end)(),
-		sigs = {
-			{
-				file = (function()
-					local ext
-					if SYSTEM.OS == "windows" then
-						ext = "zip"
-					else
-						ext = "tar.xz"
-					end
-					return "hello-" .. get_mapped_os() .. "-" .. SYSTEM.ARCH .. "." .. ext
-				end)(),
-				sig = (function()
-					local ext
-					if SYSTEM.OS == "windows" then
-						ext = "zip"
-					else
-						ext = "tar.xz"
-					end
-					return release_base_url
-							.. "/hello-"
-							.. get_mapped_os()
-							.. "-"
-							.. SYSTEM.ARCH
-							.. "."
-							.. ext
-							.. ".sig"
-				end)(),
-			},
-		},
-	},
-	{
-		name = "Build from source",
-		type = "source",
-		url = git_url,
-		platforms = { "all" },
-		build_commands = {
-			"zig build-exe main.zig -O ReleaseSmall --name hello",
-		},
-		bin_path = (function()
-			local bin
-			if SYSTEM.OS == "windows" then
-				bin = "hello.exe"
-			else
-				bin = "hello"
-			end
-			return bin
-		end)(),
-	},
+	types = { "source", "pre-compiled" },
 })
 
 dependencies({
@@ -136,3 +46,84 @@ dependencies({
 		required = { "native:zig" },
 	},
 })
+
+function prepare()
+	print("Adding PGP key for verification...")
+	addPgpKey("https://zillowe.pages.dev/keys/zillowe-main.asc", "zillowe-main")
+
+	if BUILD_TYPE == "pre-compiled" then
+		local ext
+		if SYSTEM.OS == "windows" then
+			ext = "zip"
+		else
+			ext = "tar.xz"
+		end
+		local file_name = "hello-" .. get_mapped_os() .. "-" .. SYSTEM.ARCH .. "." .. ext
+		local url = release_base_url .. "/" .. file_name
+
+		UTILS.EXTRACT(url, "precompiled")
+	elseif BUILD_TYPE == "source" then
+		cmd("git clone " .. PKG.git .. " " .. BUILD_DIR .. "/source")
+		cmd("cd " .. BUILD_DIR .. "/source && zig build-exe main.zig -O ReleaseSmall --name hello")
+	end
+end
+
+function package()
+	local bin_name = "hello"
+	if SYSTEM.OS == "windows" then
+		bin_name = "hello.exe"
+	end
+
+	if BUILD_TYPE == "pre-compiled" then
+		local bin_path = UTILS.FIND.file("precompiled", bin_name)
+		if bin_path then
+			zcp(bin_path, "${pkgstore}/bin/" .. bin_name)
+		else
+			error("Could not find '" .. bin_name .. "' in pre-compiled archive.")
+		end
+	elseif BUILD_TYPE == "source" then
+		zcp("source/" .. bin_name, "${pkgstore}/bin/" .. bin_name)
+	end
+end
+
+function verify()
+	if BUILD_TYPE == "pre-compiled" then
+		local checksum_url = release_base_url .. "/checksums-512.txt"
+		local checksum_content = UTILS.FETCH.url(checksum_url)
+
+		local ext
+		if SYSTEM.OS == "windows" then
+			ext = "zip"
+		else
+			ext = "tar.xz"
+		end
+		local file_name = "hello-" .. get_mapped_os() .. "-" .. SYSTEM.ARCH .. "." .. ext
+		local file_path = BUILD_DIR .. "/" .. file_name
+
+		local expected_checksum = UTILS.PARSE.checksumFile(checksum_content, file_name)
+
+		if not expected_checksum or not verifyHash(file_path, "sha512-" .. expected_checksum) then
+			print("Checksum verification failed!")
+			return false
+		end
+		print("Checksum verified successfully.")
+
+		print("Verifying signature...")
+		local sig_url = release_base_url .. "/" .. file_name .. ".sig"
+		local sig_path = BUILD_DIR .. "/" .. file_name .. ".sig"
+
+		print("Downloading signature from " .. sig_url)
+		UTILS.FILE(sig_url, sig_path)
+
+		if not verifySignature(file_path, sig_path, "zillowe-main") then
+			print("Signature verification failed!")
+			return false
+		end
+
+		print("Signature verified successfully.")
+		return true
+	end
+	return true
+end
+
+function uninstall() end
